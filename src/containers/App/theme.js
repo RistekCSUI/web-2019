33a5c3eff00a21e469086837c645b5f77169947a
William Rumanta <rumantawilliam@yahoo.co.id>
2019-04-17 11:07:44 +0000

    Header footer

--- src/containers/App/theme.js
+++ src/containers/App/theme.js
@@ -4,19 +4,9 @@ export const theme = {
     cardRadius: "6px"
   },
   colors: {
-    black: "#000000",
-    blue: "#10496E",
-    brown: "#BA461C",
-    darkGray: "#636363",
-    gray: "#9B9B9B",
-    lightBlack: "#4A4A4A",
-    lightBlue: "#B3C0DD",
-    lightGray: "#E6E6E6",
-    navy: "#273E71",
-    orange: "#F55D25",
-    pink: "#FFF7E6",
-    red: "#D0021B",
-    white: "#FFFFFF",
-    yellow: "#DFA735"
+    primaryBlue: "#308077",
+    lightBlue: "#6DD7CB",
+    white: "#F5F5F5",
+    black: "#444444"
   }
 };
