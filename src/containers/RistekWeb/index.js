69d7f5eb5bee8c5c01794223b7d8818be39ebc76
Justin Sung <e.justin.sung@gmail.com>
2019-04-23 17:07:05 +0000

    Resolve "Create Hero and Ristek Divisions WEB-UI"

--- src/containers/RistekWeb/index.js
+++ src/containers/RistekWeb/index.js
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import MainInfo from "../../components/MainInfo";
 import MainValues from "../../components/MainValues";
 import Achievement from "../../components/Achievement";
+import Banner from "../../components/Banner";
+import RistekDivisionSection from "../../components/RistekDivisionSection";
 
 class RistekWeb extends Component {
   render() {
     return (
       <React.Fragment>
+        <Banner />
         <MainInfo />
         <MainValues />
+        <RistekDivisionSection />
         <Achievement />
       </React.Fragment>
     );
