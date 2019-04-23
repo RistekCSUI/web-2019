f3711bcbb8f1fe9a9b7faad5ca5c072981f53440
William Rumanta <williamrumanta@gmail.com>
2019-04-24 01:22:36 +0700

    Hotfix - improve footer styling

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -9,7 +9,7 @@ export const FooterContainer = styled.div`
   }
 
   .padding-container {
-    padding: 3.5rem 3rem 1rem 3rem;
+    padding: 7.5rem 3rem 2rem 3rem;
   }
 
   .upper {
@@ -19,13 +19,13 @@ export const FooterContainer = styled.div`
   }
 
   .contact h1 {
-    font-size: 64px;
+    font-size: 80px;
   }
 
   .contact h2 {
     color: ${props => props.theme.colors.lightBlue};
     margin-top: 1rem;
-    font-size: 36px;
+    font-size: 48px;
   }
 
   .social-media {
@@ -34,7 +34,11 @@ export const FooterContainer = styled.div`
   }
 
   .social-media h2 {
-    font-size: 36px;
+    font-size: 48px;
+  }
+
+  .social-media p {
+    font-size: 28px;
   }
 
   .media-list div {
@@ -53,11 +57,11 @@ export const FooterContainer = styled.div`
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-    padding-top: 5rem;
+    padding-top: 12rem;
   }
 
   .conquerlogo {
-    width: 10rem;
+    width: 13rem;
   }
 
   .web-text {
@@ -70,46 +74,191 @@ export const FooterContainer = styled.div`
     text-align: right;
   }
 
-  .white-logoset {
-    margin-left: 1.5rem;
-    width: 8rem;
+  .web-text p {
+    font-size: 20px;
+    font-weight: bold;
+    line-height: 1.5rem;
   }
 
-  @media (max-width: 1360px) {
+  .white-logoset {
+    margin-left: 1.5rem;
+    width: 11rem;
+  }
+
+  @media (max-width: 1440px) {
+    .padding-container {
+      padding: 6.5rem 3rem 2rem 3rem;
+    }
+
+    .lower {
+      padding-top: 10rem;
+    }
+
     .social-media {
-      margin-right: 0rem;
+      margin-right: 1rem;
     }
 
     .contact h1 {
-      font-size: 56px;
+      font-size: 72px;
     }
 
     .contact h2 {
-      font-size: 28px;
+      font-size: 36px;
     }
 
     .social-media h2 {
-      font-size: 28px;
+      font-size: 36px;
+    }
+
+    .web-text p {
+      font-size: 18px;
+    }
+
+    .conquerlogo {
+      width: 11rem;
+    }
+
+    .white-logoset {
+      width: 10rem;
     }
   }
 
   @media (max-width: 1280px) {
+    .social-media {
+      margin-right: 0rem;
+    }
+
     .padding-container {
-      padding: 3rem 2rem 1rem 2rem;
+      padding: 6.5rem 2rem 2rem 2rem;
     }
   }
 
   @media (max-width: 1024px) {
     .contact h1 {
+      font-size: 62px;
+    }
+
+    .contact h2 {
+      font-size: 30px;
+    }
+
+    .social-media h2 {
+      font-size: 30px;
+    }
+
+    .conquerlogo {
+      width: 10rem;
+    }
+
+    .white-logoset {
+      width: 8rem;
+    }
+
+    .social-media p {
+      font-size: 22px;
+    }
+
+    .web-text p {
+      font-size: 16px;
+    }
+
+    .media-list img {
+      width: 2rem;
+    }
+  }
+
+  @media (max-width: 768px) {
+    .padding-container {
+      padding: 6.5rem 3rem 3rem 3rem;
+    }
+
+    .upper {
+      flex-direction: column;
+    }
+
+    .lower {
+      padding-top: 4rem;
+    }
+
+    .contact h2 {
+      font-size: 42px;
+    }
+
+    .social-media h2 {
+      font-size: 42px;
+    }
+
+    .white-logoset {
+      margin-left: 0.5rem;
+    }
+
+    .contact,
+    .social-media {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+    }
+
+    .contact {
+      padding-bottom: 2.5rem;
+    }
+
+    .social-media h2 {
+      margin-bottom: 1rem;
+    }
+  }
+
+  @media (max-width: 600px) {
+    .contact h1 {
+      font-size: 56px;
+    }
+
+    .contact h2 {
+      font-size: 36px;
+    }
+
+    .social-media h2 {
+      font-size: 36px;
+    }
+
+    .conquerlogo {
+      width: 9rem;
+    }
+
+    .white-logoset {
+      width: 7rem;
+    }
+
+    .social-media p {
+      font-size: 20px;
+    }
+
+    .web-text p {
+      font-size: 15px;
+    }
+
+    .media-list img {
+      width: 2rem;
+    }
+  }
+
+  @media (max-width: 500px) {
+    .padding-container {
+      padding: 6.5rem 2rem 2.5rem 2rem;
+    }
+
+    .contact h1 {
       font-size: 48px;
     }
 
     .contact h2 {
-      font-size: 24px;
+      font-size: 28px;
     }
 
     .social-media h2 {
-      font-size: 24px;
+      font-size: 28px;
     }
 
     .conquerlogo {
@@ -120,43 +269,22 @@ export const FooterContainer = styled.div`
       width: 6rem;
     }
 
-    .media-list,
     .web-text p {
-      font-size: 14px;
-    }
-
-    .media-list img {
-      width: 2rem;
-    }
-  }
-
-  @media (max-width: 768px) {
-    .padding-container {
-      padding: 2rem 1rem 1rem 1rem;
-    }
-  }
-
-  @media (max-width: 650px) {
-    .upper {
-      flex-direction: column;
-    }
-
-    .contact {
-      margin-bottom: 3rem;
+      font-size: 12px;
     }
   }
 
   @media (max-width: 450px) {
-    .upper {
-      flex-direction: column;
-    }
-
-    .contact {
-      margin-bottom: 1rem;
+    .padding-container {
+      padding: 4rem 2rem 2rem 2rem;
     }
 
     .web-text p {
       display: none;
+    }
+
+    .contact {
+      padding-bottom: 2rem;
     }
 
     .contact h1 {
@@ -164,23 +292,24 @@ export const FooterContainer = styled.div`
     }
 
     .contact h2 {
-      font-size: 20px;
+      font-size: 22px;
+      margin-top: 0.5;
     }
 
     .social-media h2 {
       font-size: 22px;
     }
 
+    .social-media p {
+      font-size: 18px;
+    }
+
     .conquerlogo {
-      width: 7rem;
+      width: 8rem;
     }
 
     .white-logoset {
       width: 5rem;
-    }
-
-    .media-list img {
-      width: 1.5rem;
     }
   }
 `;
