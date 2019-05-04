68fe562c364bd1c11641f5a25aae8be8dec3c800
Michael Giorgio <michaelgwirawan@gmail.com>
2019-05-04 13:37:24 +0000

    Achievements

--- src/components/Achievement/style.js
+++ src/components/Achievement/style.js
@@ -6,17 +6,26 @@ export const AchievementContainer = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    color: #444444;
+    color: ${props => props.theme.colors.black};
     margin: 5rem 0 5rem 0;
+    padding: 1rem;
   }
 
-  .main div {
-    margin: 0 0 1rem 0;
+  .main .main-body,
+  .main .upper-title,
+  .lower-title {
+    margin: 0 0 2rem 0;
+  }
+
+  .upper-title h1 {
+    font-size: 50px;
+    text-align: center;
   }
 
   .lower-title {
     width: 70vw;
     text-align: center;
+    font-size: 20px;
   }
 
   .main-body {
@@ -33,5 +42,20 @@ export const AchievementContainer = styled.div`
     flex-wrap: wrap;
     justify-content: center;
     align-items: center;
+  }
+
+  @media only screen and (max-width: 1024px) {
+    .main {
+      margin: 2rem 0;
+      padding: 0;
+    }
+
+    .upper-title h1 {
+      font-size: 38px;
+    }
+
+    .lower-title p {
+      font-size: 16px;
+    }
   }
 `;
