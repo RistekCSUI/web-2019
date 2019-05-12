41f396591d95c75f26d51f70d21ee8092780cc0d
William Rumanta <rumantawilliam@yahoo.co.id>
2019-05-13 02:26:59 +0700

    Hotfix - Add hyperlink to contacts

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React, { Component } from "react";
 import { FooterContainer } from "./style";
 import ConquerTechLogo from "../../assets/conquertech.svg";
@@ -15,22 +16,36 @@ class Footer extends Component {
             <div className="upper">
               <div className="contact">
                 <h1>Contact us at</h1>
-                <h2>ristekfasilkomui@gmail.com</h2>
+                <a href="mailto:ristekfasilkomui@gmail.com" target="_blank">
+                  ristekfasilkomui@gmail.com
+                </a>
               </div>
               <div className="social-media">
                 <h2>Or find us through social media</h2>
                 <div className="media-list">
                   <div>
                     <img className="social-icon" src={LineLogo} />
-                    <p>@ristekfasilkomui</p>
+                    <a>@ristekfasilkomui</a>
                   </div>
-                  <div>
+                  <div
+                    className="clickable"
+                    onClick={() =>
+                      window.open("https://twitter.com/ristekcsui")
+                    }
+                  >
                     <img className="social-icon" src={TwitterLogo} />
-                    <p>@RistekCSUI</p>
+                    <a href="https://twitter.com/ristekcsui" target="_blank">
+                      @RistekCSUI
+                    </a>
                   </div>
-                  <div>
+                  <div
+                    className="clickable"
+                    onClick={() => window.open("https://fb.me/RistekCSUI")}
+                  >
                     <img className="social-icon" src={FacebookLogo} />
-                    <p>Ristek Fasilkom UI</p>
+                    <a href="https://fb.me/RistekCSUI" target="_blank">
+                      Ristek Fasilkom UI
+                    </a>
                   </div>
                 </div>
               </div>
@@ -40,9 +55,13 @@ class Footer extends Component {
                 <img className="conquerlogo" src={ConquerTechLogo} />
               </div>
               <div className="web-text">
-                <div>
-                  <p>ristek.cs.ui.ac.id</p>
-                  <p>fb.me/RistekCSUI</p>
+                <div className="link-container">
+                  <a href="https://ristek.cs.ui.ac.id" target="_blank">
+                    ristek.cs.ui.ac.id
+                  </a>
+                  <a href="https://fb.me/RistekCSUI" target="_blank">
+                    fb.me/RistekCSUI
+                  </a>
                 </div>
                 <img className="white-logoset" src={WhiteLogoset} />
               </div>
