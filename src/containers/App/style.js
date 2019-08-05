00745c107e53b434dac291b0e52fade75076d9aa
William Rumanta <rumantawilliam@yahoo.co.id>
2019-08-05 22:10:03 +0700

    Fix unapplied Proxima Nova font-family in body

--- src/containers/App/style.js
+++ src/containers/App/style.js
@@ -2,15 +2,6 @@
 import styled from "styled-components";
 
 export const AppContainer = styled.div`
-  @font-face {
-    font-family: "Proxima Nova";
-    src: url("assets/proximanova.ttf") .format("truetype");
-    font-weight: normal;
-    font-style: normal;
-  }
-
-  font-family: "Proxima Nova", sans-serif;
-
   div,
   h1,
   h2,
